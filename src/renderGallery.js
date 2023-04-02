774d132034cd3fc7d1be132cc240c2faf59a408d
OlgaTikhonchik <olga882011@gmail.com>
2023-04-02 17:36:53 +0300

    Изменение стилей

--- src/renderGallery.js
+++ src/renderGallery.js
@@ -12,17 +12,19 @@ function renderGallery(images) {
         downloads,
       } = image;
       return `
-        <a class="gallery__link" href="${largeImageURL}">
-          <div class="gallery-item" id="${id}">
-            <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-            <div class="info">
-              <p class="info-item"><b>Likes</b>${likes}</p>
-              <p class="info-item"><b>Views</b>${views}</p>
-              <p class="info-item"><b>Comments</b>${comments}</p>
-              <p class="info-item"><b>Downloads</b>${downloads}</p>
+        <div class = "gallery-card">
+          <a class="gallery__link" href="${largeImageURL}">
+            <div class="gallery-item" id="${id}">
+              <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" width="320" height="200" />
+              <div class="info">
+                <p class="info-item"><b>Likes</b>${likes}</p>
+                <p class="info-item"><b>Views</b>${views}</p>
+                <p class="info-item"><b>Comments</b>${comments}</p>
+                <p class="info-item"><b>Downloads</b>${downloads}</p>
+              </div>
             </div>
-          </div>
-        </a>
+          </a>
+        </div>
       `;
     })
     .join('');
